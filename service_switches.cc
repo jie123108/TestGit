9334559cbcc3a5a8cce4f8cf21c067dca75c0faa
jie123108 <jie123108@163.com>
2014-03-08 21:40:27 +0800

    好啊。。

--- service_switches.cc
+++ service_switches.cc
@@ -13,3 +13,15 @@ const char kStartSwitch[] = "start";
 const char kStopSwitch[] = "stop";
 const char kUninstallSwitch[] = "uninstall";
 
+
+asdfsadf
+
+
+static int ngx_http_lua_shdict_set(lua_State *L);
+static int ngx_http_lua_shdict_safe_set(lua_State *L);
+static int ngx_http_lua_shdict_get(lua_State *L);
+static int ngx_http_lua_shdict_expire(ngx_http_lua_shdict_ctx_t *ctx,
+    ngx_uint_t n);
+static ngx_int_t ngx_http_lua_shdict_lookup(ngx_shm_zone_t *shm_zone,
+    ngx_uint_t hash, u_char *kdata, size_t klen,
+    ngx_http_lua_shdict_node_t **sdp);
